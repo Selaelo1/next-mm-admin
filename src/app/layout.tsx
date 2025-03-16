404e46217b896a3fef90185519fea50b9d681eb2
Selaelo2 <selaelolanga10@gmail.com>
2025-03-16 18:40:44 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
 import { Box } from '@chakra-ui/react';
 import React, { ReactNode } from 'react';
 import AppWrappers from './AppWrappers';
+import WalletConnection from '../components/WalletConnection';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body id={'root'}>
-        <AppWrappers>{children}</AppWrappers>
+        <AppWrappers>
+          {/* Add Wallet Connection Button and Addresses */}
+          <WalletConnection />
+          {children}
+        </AppWrappers>
       </body>
     </html>
   );
